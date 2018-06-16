1a22003567afd786d045d11246595f6a2e44090f
Camilo <Camilo>
2018-06-16 15:39:23 -0500

    Service to open Human-lab

---
+++ WebABI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebABI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebABI/Controllers/ABIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebABI.Controllers
+{
+    public class AbiController : ApiController
+    {
+        // GET: ABI
+
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public void Post(int id,[FromBody]string value)
+        {
+            Console.WriteLine("ID:" + id);
+            Process process = new Process();
+            try
+            {
+                process.StartInfo.FileName = "C:\\Users\\CamiloAndrés\\source\\repos\\software analizador de movimiento\\Human-Lab.exe";
+                process.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+          
+
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+    }
+}
